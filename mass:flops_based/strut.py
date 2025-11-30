4020f2f6f69ffa8d3763928ee93321b14c4e580b
carissa-kamm <carissa.kamm@gmail.com>
2025-11-30 14:49:37 -0800

    updated ./mass/flops_based with strut mass

---
+++ mass:flops_based/strut.py
@@ -0,0 +1,50 @@
+import openmdao.api as om
+
+from aviary.constants import GRAV_ENGLISH_LBM
+from aviary.variable_info.functions import add_aviary_input, add_aviary_output
+from aviary.variable_info.variables import Aircraft, Mission
+
+class StrutMass(om.ExplicitComponent):
+    """
+    Calculates the mass of the strut.
+    """
+
+    def setup(self):
+        add_aviary_input(self, Mission.Design.GROSS_MASS, units='lbm')
+        add_aviary_input(self, Aircraft.Strut.AREA, units='ft**2')
+        add_aviary_input(self, Aircraft.Strut.LENGTH, units='ft')
+
+
+        add_aviary_output(self, Aircraft.Strut.MASS, units='lbm')
+
+    def setup_partials(self):
+        self.declare_partials('*', '*')
+
+    def compute(self, inputs, outputs):
+        # togw = inputs[Mission.Design.GROSS_MASS] * GRAV_ENGLISH_LBM
+        area = inputs[Aircraft.Strut.AREA]
+        length = inputs[Aircraft.Strut.LENGTH]
+        rho = 170       # density [lm/ft^3]
+
+        strut_weight = area * length * rho
+        outputs[Aircraft.Strut.MASS] = (
+            strut_weight / GRAV_ENGLISH_LBM
+        )
+
+    def compute_partials(self, inputs, J):
+        area = inputs[Aircraft.Strut.AREA]
+        length = inputs[Aircraft.Strut.LENGTH]
+        rho = 170       # density [lm/ft^3]
+        # taper_ratio = inputs[Aircraft.Canard.TAPER_RATIO]
+        # scaler = inputs[Aircraft.Canard.MASS_SCALER]
+        # gross_weight = inputs[Mission.Design.GROSS_MASS] * GRAV_ENGLISH_LBM
+
+        # gross_weight_exp = gross_weight**0.2
+
+        J[Aircraft.Strut.MASS, Aircraft.Strut.AREA] = (
+                (rho * length) / GRAV_ENGLISH_LBM
+        )
+
+        J[Aircraft.Strut.MASS, Aircraft.Strut.LENGTH] = (
+                (rho * area) / GRAV_ENGLISH_LBM
+        )
